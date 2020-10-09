2f3505842027c982b912159f7379b8237d8b8a77
Rodolfo Santos <msa.rodolfo@gmail.com>
2020-10-09 03:40:58 -0400

    tela home semi pronta

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -39,38 +39,55 @@
         </v-btn>
     </v-app-bar> 
 
-    <center><v-row>
-     
-      <v-col>     
-        <v-img
-          :src="require('../assets/book.jpeg')"
-          contain
-          height="300"
-          width="300"
-        />
-        <p> Descrição Filmes </p>
-      </v-col>
+    <v-container>
+      <v-row> 
+        <v-col>     
+          <img
+            src="../assets/book.jpeg"
+            contain
+            height="300"
+            width="300"
+            style="border-radius: 10px"
+          />
+          <p> Descrição Livro </p>
+        </v-col>
 
-      <v-col><br>   
-        <p>bla bla</p>  
-        <v-img
-          :src="require('../assets/book.jpeg')"
-          contain
-          height="300"
-          width="300"
-        />
-      </v-col>
+        <v-col><br>   
+          <p>Descrição Filme</p>  
+          <img
+            :src="require('../assets/movie.jpeg')"
+            contain
+            height="300"
+            width="300"
+            style="border-radius: 10px"
+          />
+        </v-col>
 
-      <v-col>     
-        <v-img
-          :src="require('../assets/book.jpeg')"
-          contain
-          height="300"
-          width="300"
-        />
-        <p>bla bla</p>
-      </v-col>
-    </v-row></center>
+        <v-col>     
+          <img
+            :src="require('../assets/serie.jpeg')"
+            contain
+            height="300"
+            width="300"
+            style="border-radius: 10px"
+          />
+          <p>Descrição Serie</p>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-card height="200">
+      <v-footer
+        absolute
+        class="font-weight-medium"
+      >
+        <v-col
+          class="text-center"
+          cols="12">
+          <h5>© 2020 MiCs Minha Criticas. Todos os direitos reservados.</h5>
+        </v-col>
+      </v-footer>
+    </v-card>
   </div>
 </template>
 
